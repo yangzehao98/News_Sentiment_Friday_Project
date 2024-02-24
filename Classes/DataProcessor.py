348a5f30b0112daab453c9c2431148d5adea8104
Zehao Yang <yangzehao98@163.com>
2024-02-23 22:57:16 -0500

    UpdatedDataProcessor

--- Classes/DataProcessor.py
+++ Classes/DataProcessor.py
@@ -1,17 +1,18 @@
+
 class Tokenizer:
-    pass
+    #
 
 
 class NewsTokenizer:
     pass
-
+ 
 
 class NewsSentimentAnalysis:
     pass
 
 
 class SentenceSentimentAnalysis:
-    pass
+    #
 
 
 class Counter:
